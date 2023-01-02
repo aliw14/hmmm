ab7ecadba92c72d3c2135dbcc8156cc618f08cc1
ali <ali@MacBook-Pro-Ali.local>
2023-01-02 19:02:36 +0400

    mesaj

---
+++ hmmm/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace hmmm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int count = 0;
+        int[] numbers = { 5, 7, 10, 15 };
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 5 == 0)
+            {
+                sum += numbers[i];
+                count++;
+            }
+        }Console.WriteLine(sum);
+        Console.WriteLine(count);
+
+        
+    }
+}
+
